a0ad640f69f04c33fe432dd41667e030516f98db
rubberbandboy <30496864+rubberbandboy@users.noreply.github.com>
2020-05-23 14:20:00 +0100

    Update scan.sh
    remove pointless echo "" commands - annyoying as I sent to a syslog server

--- scan.sh
+++ scan.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 echo "Updating ClamAV Scanner versions"
 apk update
 apk upgrade
-echo ""
 echo "$( date -I'seconds' ) ClamAV process starting"
-echo ""
 echo "Updating ClamAV scan DB"
 set +e
 freshclam
@@ -15,24 +13,14 @@ set -e
 echo ""
 
 if [[ "$FRESHCLAM_EXIT" -eq "0" ]]; then
-    echo ""
     echo "Freshclam updated the DB"
-    echo ""
 elif [[ "$FRESHCLAM_EXIT" -eq "1" ]]; then
-    echo ""
     echo "ClamAV DB already up to date..."
-    echo ""
 else
-    echo ""
     echo "An error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT')"
-    echo ""
     exit $FRESHCLAM_EXIT
 fi
-echo ""
 clamscan -V
-echo ""
 echo "Scanning $SCANDIR"
-echo ""
 clamscan -r --max-filesize=4000M --max-scansize=4000M --bytecode-timeout=190000 $SCANDIR $@
-echo ""
 echo "$( date -I'seconds' ) ClamAV scanning finished"
